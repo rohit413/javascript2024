8e685145ee3c8268a528aea7822bc5b4f1bd06f6
Dharmendra Kumar Singh <dksingh413@gmail.com>
2024-06-30 20:03:39 +0000

    datatype conversion test

---
+++ jsone/js_conversionOperation.js
@@ -0,0 +1,11 @@
+// let score= "33abc"
+let score= "rohit"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
