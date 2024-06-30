e3e5255ca1ea6869f05bcadf29cd36c9e2594399
Dharmendra Kumar Singh <dksingh413@gmail.com>
2024-06-30 19:32:20 +0000

    update var let const

---
+++ jsone/js_var_let_const.js
@@ -0,0 +1,19 @@
+//Day One 01-07-2024
+// console.log("hello Rohit");
+
+const accountId =  12345;
+let accountEmail =  "rohit@gmail.com"
+var accountPassword = "12345"
+accountCity = "Bihar"
+
+//accountId = 2 // Not allowed 
+accountEmail = "abc@gmail.com"
+accountPassword = "54321"
+accountCity = "Delhi"
+
+
+console.table([accountId,accountEmail,accountCity]);
+
+
+
+
